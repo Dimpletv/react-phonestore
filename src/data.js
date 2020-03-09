9feaf804abbc966d622a6430f194153c81d0d112
Dimpletv <tvdimple@gmail.com>
2020-03-09 10:05:57 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 70,
+    company: "GOOGLE",
+    info:
+      "Renewed products work and look like new. These pre-owned products have been inspected and tested by qualified suppliers. Box and accessories (no headphones included) may be generic. 18W Power Adapter and USB-C to USB-C Cable. No other accessories are included",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 95,
+    company: "SAMSUNG",
+    info:
+      "Renewed products work and look like new. These pre-owned products have been inspected and tested by qualified suppliers. Box and accessories (no headphones included) may be generic. 5.1-inch QHD sAMOLED, 1440 x 2560 pixels with Corning Gorilla Glass 4.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 120,
+    company: "htc",
+    info:
+      "HTC Desire 10 Pro comes with a superb primary camera of 20 MP , f/2.2 , 28mm , laser autofocus , dual-LED (dual tone) flash, It gives you a wonderful features as Auto HDR, panorama, face detection, touch focus, geo-tagging and it takes wonderful photos.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 130,
+    company: "htc",
+    info:
+      "HTC Desire 10 Pro comes with a superb primary camera of 20 MP , f/2.2 , 28mm , laser autofocus , dual-LED (dual tone) flash, It gives you a wonderful features as Auto HDR, panorama, face detection, touch focus, geo-tagging and it takes wonderful photos.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 60,
+    company: "htc",
+    info:
+      "4G LTE Single SIM (4G does not work in the US); 3G works with AT&T; DOES NOT work with Sprint, Verizon, US Cellular and all other CDMA carriers; LTE compatibility: This is international stock, several per carrier.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "New: A brand-new, unused, unopened, undamaged item in its original packaging (where packaging is applicable). Packaging should be the same as what is found in a retail store, unless the item is handmade or was packaged by the manufacturer in non-retail packaging, such as an unprinted box or plastic bag.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 80,
+    company: "apple",
+    info:
+      "Renewed products work and look like new. These pre-owned products are not Apple certified but have been inspected and tested by qualified suppliers. Box and accessories (no headphones included) may be generic. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Smashed Iphone",
+    img: "img/product-8.png",
+    price: 20,
+    company: "apple",
+    info:
+      "Renewed products work and look like new. These pre-owned products are not Apple certified but have been inspected and tested by qualified suppliers. Box and accessories (no headphones included) may be generic. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Renewed products work and look like new. These pre-owned products have been inspected and tested by qualified suppliers. Box and accessories (no headphones included) may be generic. 18W Power Adapter and USB-C to USB-C Cable. No other accessories are included.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
